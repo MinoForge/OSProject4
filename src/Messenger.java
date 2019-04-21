60db965d111992f783b33df736ea78c22579b04a
MinoForge <rendrag6@bellsouth.net>
2019-04-20 22:55:25 -0400

    Messenger started

--- src/Messenger.java
+++ src/Messenger.java
@@ -1,2 +1,18 @@
+import mining.Miner;
+import mining.Supplies;
+
 public class Messenger {
+
+    private Docks dock;
+    private Miner miner;
+    private Supplies[] observe;
+
+    public Messenger(Docks dock, Miner miner, Supplies[] observe) {
+        this.dock = dock;
+        this.miner = miner;
+        this.observe = observe;
+    }
+
+
+
 }
