b92a07922bde355ad56ddffb717e0c0e23970754
MinoForge <rendrag6@bellsouth.net>
2019-04-20 22:08:21 -0400

    Docks

--- src/Docks.java
+++ src/Docks.java
@@ -1,2 +1,24 @@
 public class Docks {
+
+    private Object breadMiners;
+    private Object bolognaMiners;
+    private Object cheeseMiners;
+
+    public Docks() {
+        this.breadMiners = new Object();
+        this.bolognaMiners = new Object();
+        this.cheeseMiners = new Object();
+    }
+
+    public Object getBreadMiners() {
+        return breadMiners;
+    }
+
+    public Object getBolognaMiners() {
+        return bolognaMiners;
+    }
+
+    public Object getCheeseMiners() {
+        return cheeseMiners;
+    }
 }
