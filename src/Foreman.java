b4861b8a0f976bc61157cd3eef8c22b343f5b032
Wesley Rogers <wsrogers3@catamount.wcu.edu>
2019-04-20 22:55:21 -0400

    woo

--- src/Foreman.java
+++ src/Foreman.java
@@ -11,6 +11,8 @@ public class Foreman implements Runnable {
 
     private final Docks theDock;
 
+    private volatile boolean running;
+
     public Foreman(Docks docks) {
         this.types = Supplies.values();
         theDock = docks;
@@ -24,16 +26,34 @@ public class Foreman implements Runnable {
          * Foreman will NOT send any more supplies of any kind until the two picked have been
          * consumed.
          */
-        Random threadRng = ThreadLocalRandom.current();
-        int type1Index = threadRng.nextInt(types.length);
-        int type2Index = (type1Index + threadRng.nextInt(types.length - 1)) % types.length;
-        Supplies type1 = types[type1Index];
-        Supplies type2 = types[type2Index];
+        while(running) {
 
-        synchronized (theDock) {
-            theDock.notify();
+            Random threadRng = ThreadLocalRandom.current();
+            int type1Index = threadRng.nextInt(types.length);
+            // start at index 1, then go a random amount around in a loop, without making a full
+            // loop
+            int type2Index = (type1Index + threadRng.nextInt(types.length - 2) + 1) % types.length;
+            Supplies type1 = types[type1Index];
+            Supplies type2 = types[type2Index];
+
+            synchronized (theDock) {
+                // deliver to the dock
+                theDock.deliver(type1);
+                theDock.deliver(type2);
+                // wake the dock up and say I gave it something
+                theDock.notify();
+            }
+
+            synchronized (theDock) {
+                try {
+                    // wait for the dock to be empty
+                    theDock.wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Foreman was interrupted, goodbye.");
+
+                }
+            }
         }
-
 
 
     }
